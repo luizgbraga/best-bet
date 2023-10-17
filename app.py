c664ae29601feb15d0127c033dc94a1d09ba6b86
luizgbraga <luizb.07@outlook.com>
2023-10-17 09:03:10 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import request, render_template, send_from_directory, redirect, url_for, flash
+from flask import session
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/example', methods=['GET', 'POST'])
+def leads():
+    if request.method == 'POST':
+        email = request.form['email']
+        # access to every variable passed on POST request
+        print(email)
+        return render_template('example.html', value=email)
+    
+    return render_template('example.html', variable='hello', list=[1, 2, 3, 4, 5])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
