25e72cedc3ee8d9dfc9709ec9dd30d7a43193e6d
luizgbraga <luizb.07@outlook.com>
2023-11-07 11:41:45 -0300

    correcting bad flask redirect

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request, render_template, send_from_directory, redirect, url_for, flash
+from flask import request, render_template, send_from_directory, redirect, url_for, flash, redirect
 from flask import session
 
 from data.collect_data import DataCollector
@@ -8,6 +8,7 @@ from util.time_converter import TimeConverter
 from network.network import Network
 
 app = Flask(__name__)
+app.secret_key = 'abcd'
 
 CSV_NAME = 'brasileirao.csv'
 
@@ -27,13 +28,17 @@ def inputs():
         input_data = data_processor.format_input_data(home_team_id, visit_team_id, hour, round)
         net = Network([len(input_data), 60, 9])
         train, test = data_processor.generate_tuples()
-        print(train[0], test[0])
         net.SGD(train, 30, 10, 3.0, test_data=test)
         output = net.feedforward(input_data)
         bets = data_processor.output_to_list(output)
-        return render_template('outputs.html', bets=bets)
+        session['bets'] = ','.join(bets)
+        return redirect(url_for('outputs'))
     
     return render_template('inputs.html', home_teams=data_collector.get_club_names(), visit_teams=data_collector.get_club_names(), hours=TimeConverter.all_times())
+
+@app.route('/outputs', methods=['GET'])
+def outputs():
+    return render_template('outputs.html', bets=session['bets'].split(','))
 
 if __name__ == "__main__":
     app.run(debug=True)
