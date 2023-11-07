78fbc8dc07c5a4e57322e20e3a0892fe0af95eaf
luizgbraga <luizb.07@outlook.com>
2023-11-07 08:15:07 -0300

    testing network

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import session
 from data.collect_data import DataCollector
 from data.process_data import DataProcessor
 from util.time_converter import TimeConverter
+from network.network import Network
 
 app = Flask(__name__)
 
@@ -21,11 +22,17 @@ def inputs():
         hour = TimeConverter.str_to_float(request.form['hour'])
         round = int(request.form['round'])
         input_data = DataProcessor.format_input_data(home_team_id, visit_team_id, hour, round)
-        print(input_data)
+        net = Network([len(input_data), 60, 9])
+        training_data = DataCollector.generate_training_and_test_data()
+        train = DataProcessor.generate_tuple(training_data['input_training_data'], training_data['output_training_data'])
+        test = DataProcessor.generate_tuple(training_data['input_testing_data'], training_data['output_testing_data'])
+        print(train, test)
+        net.SGD(train, 30, 10, 3.0, test_data=test)
+        output = net.feedforward(input_data)
+        print(output)
         return render_template('inputs.html')
     
     return render_template('inputs.html', home_teams=clubs, visit_teams=clubs, hours=TimeConverter.all_times())
-
 
 if __name__ == "__main__":
     app.run(debug=True)
